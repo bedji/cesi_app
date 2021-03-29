a63417589ff37ae45389841e19786dd3ee7f0d9a
bedji <badrou14@yahoo.fr>
2021-03-29 08:49:37 +0200

    add lien

--- promos.php
+++ promos.php
@@ -109,7 +109,9 @@ $promos = $req->fetchAll();
                       <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Jessica Doe">
                         <img alt="Image placeholder" src="./assets/img/theme/team-4.jpg">
                       </a>
-
+                      <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Jessica Doe">
+                        <img alt="Image placeholder" src="./assets/img/theme/team-4.jpg">
+                      </a>
                       <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Jessica Doe">
                         <img alt="Image placeholder" src="./assets/img/theme/team-4.jpg">
                       </a>
